0fedfe64c6176bd704f465c22455c4156551c7cc
sohan <sohan@uleeui-MacBookAir.local>
2021-07-15 04:49:12 +0900

    test

--- ft_printf.h
+++ ft_printf.h
@@ -35,7 +35,7 @@ enum	e_flags
 	hex_low = 1 << 2,
 	hex_up = 1 << 1,
 	percent = 1 << 0,
-	malloc_free = 0b0000000111110,
+	malloc_free = 0b0000000000111110,
 	pointer_len_fixed = 11
 };
 
@@ -50,4 +50,5 @@ typedef	struct s_component
 	int		flag;
 }				t_component;
 
+int	ft_printf(const char *fmt, ...);
 #endif
---
+++ printf.c
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "ft_printf.h"
+
+int	ft_printf(const char *fmt, ...)
+{
+	return (-1);
+}
